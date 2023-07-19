dc5423b6106f39c0bf24efea52f063ca59fd689f
ThaTha2304 <taufiqagung23@gmail.com>
2023-07-19 22:04:11 +0700

    Menambah file episode 4: pengenalan variabel

---
+++ 04. Pengenalan Variabel/pengenalan_variabel.py
@@ -0,0 +1,24 @@
+# Variabel di Python, sama kayak variabel di matematika/bahasa pemrograman yang lain
+
+# Assign nilai
+a = 10
+nilai_x = 15
+zScore = 3.15
+
+'''
+Ini adalah comment multi-line
+Penamaan variabel:
+    nilai x = 10 --> gak bisa, gak boleh ada spasi
+    10ribu = 10000 --> gak bisa, angka tidak boleh di awal
+    nilai-x = 10.9 --> gak bisa, gak boleh pake tanda minus
+'''
+
+print("Nilai a =",a) # Output = 10
+
+#Re-assign nilai variabel
+a = 7
+print("Nilai a =",a) # Output = 7
+
+# Indirect assignment
+b = a
+print("Nilai b =",b) # Output = 7
