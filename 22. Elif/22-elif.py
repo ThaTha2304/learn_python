6c0b7fdb797886fdd64c2d3bec742dd1baf98449
ThaTha2304 <taufiqagung23@gmail.com>
2023-07-25 22:35:18 +0700

    Menambahkan file Episode 22: Elif (Else-If)

---
+++ 22. Elif/22-elif.py
@@ -0,0 +1,11 @@
+nama = input("Masukkan nama anda: ")
+
+if nama == "ThaTha":
+    print("Hello ThaTha~~")
+elif nama == "Awan":
+    print("Wuiidiii.. ada Awan nichh...")
+elif nama == "Agung":
+    print("Mmmm... Naissee...~~")
+else:
+    print("Ketjee Abieez!")
+print("\nEnd of Program...")
