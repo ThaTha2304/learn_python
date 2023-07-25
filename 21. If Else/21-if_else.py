040e8b9954733731ecbdaf78f294341a2939ab23
ThaTha2304 <taufiqagung23@gmail.com>
2023-07-25 22:20:13 +0700

    Menambahkan file Episode 21: If Else

---
+++ 21. If Else/21-if_else.py
@@ -0,0 +1,20 @@
+# If Else Statement
+
+nama = input("Masukkan nama anda: ")
+
+# 1. If statement ==> inline
+if nama=="ThaTha": print("Wahh, kamu ThaTha :D")
+print("End of Inline Statement...\n")
+
+# 2. If statement ==> indentation
+if nama == "ThaTha":
+    print("Woahh, ketjee abieez!")
+    print("Mantap kalii banhh~")
+print("End of Indentation Statement...\n")
+
+# If-else Statement
+if nama == "ThaTha":
+    print("Haii ThaTha! :D")
+else:
+    print("Hmmm... kamu bukan ThaTha :\"D")
+print("End of If-Else statement")
